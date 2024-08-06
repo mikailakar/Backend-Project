713cd4bce16063f0c3fbe51e34c6be5667bbcd33
Mikail Akar <huseyin.akar@bil.omu.edu.tr>
2024-08-06 13:25:30 +0300

    Database ve Tablo

---
+++ backendProjesi/Controllers/UsersController.cs
@@ -0,0 +1,98 @@
+﻿using backendProjesi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendProjesi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersContext _usersContext;
+        public UsersController(UsersContext usersContext)
+        {
+            _usersContext = usersContext;
+        }
+
+        // Get : api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            if (_usersContext.Users == null)
+            {
+                return NotFound();
+            }
+            return await _usersContext.Users.ToListAsync();
+        }
+
+        // Get : api/Users/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(int id)
+        {
+            if (_usersContext.Users is null)
+            {
+                return NotFound();
+            }
+            var users = await _usersContext.Users.FindAsync(id);
+            if (users is null)
+            {
+                return NotFound();
+            }
+            return users;
+        }
+
+        // Post : api/Users
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostUsers(Users users)
+        {
+            _usersContext.Users.Add(users);
+            await _usersContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUsers), new { id = users.Id }, users);
+        }
+
+        // Put : api/Users/2
+        [HttpPut]
+        public async Task<ActionResult<Users>> PutUsers(int id, Users users)
+        {
+            if (id != users.Id)
+            {
+                return BadRequest();
+            }
+            _usersContext.Entry(users).State = EntityState.Modified;
+            try
+            {
+                await _usersContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id)) { return NotFound(); }
+                else { throw; }
+            }
+            return NoContent();
+        }
+
+        private bool UsersExists(long id)
+        {
+            return (_usersContext.Users?.Any(users => users.Id == id)).GetValueOrDefault();
+        }
+
+        // Delete : api/Users/2
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Users>> DeleteUsers(int id)
+        {
+            if (_usersContext.Users is null)
+            {
+                return NotFound();
+            }
+            var users = await _usersContext.Users.FindAsync(id);
+            if (users is null)
+            {
+                return NotFound();
+            }
+            _usersContext.Users.Remove(users);
+            await _usersContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ backendProjesi/Models/UsersContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backendProjesi.Models
+{
+    public class UsersContext:DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            :base(options) { }
+
+        public DbSet<Users> Users { get; set; } = null!;
+    }
+}
